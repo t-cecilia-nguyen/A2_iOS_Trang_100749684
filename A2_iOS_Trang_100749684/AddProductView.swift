c2ee5cbdaed81cd953ca62e7b7e103ea68702c2a
Trang Nguyen <w5749684@macmini01.gbccloud.com>
2025-03-25 13:07:47 -0400

    implement add product view

---
+++ A2_iOS_Trang_100749684/AddProductView.swift
@@ -0,0 +1,23 @@
+//
+//  AddProductView.swift
+//  A2_iOS_Trang_100749684
+//
+//  Created by Trang Nguyen on 2025-03-25.
+//
+
+import Foundation
+import SwiftUI
+
+struct AddProductView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State private var name = ""
+    @State private var description = ""
+    @State private var price = ""
+    @State private var provider = ""
+    
+    var body: some View {
+        
+        }
+    }
+}
